c77099f95cd0d69ec8a65124ff67fa145c54222c
sbentley2019 <sbentley000@gmail.com>
2019-10-14 19:42:11 +0000

    Created a head.js file that returns the head of an array

---
+++ head.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✔️ ✔️ ✔️  Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`❌️❌️❌️ Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const head = function(list){
+  if(list === undefined){
+    return undefined;
+  } else if(typeof list === 'number' || list.length === 1){
+    return list;
+  } else {
+    return list[0];
+  }
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(1), 1);
+assertEqual(head([]), 1);
